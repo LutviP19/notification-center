ff30a75772c1ed5c78807c3f961585ce07bcb63e
LutviP19 <lutvip19@gmail.com>
2022-02-17 02:41:03 +0700

    add passport API Oauth2

--- public/js/encryption.js
+++ public/js/encryption.js
@@ -1,3 +1,32 @@
+import _ from 'lodash';
+
+//import crypto from 'crypto-browserify';
+import crypto from 'crypto';
+
+var key = process.env.MIX_OPENSSL_SECRET_KEY;
+var iv = process.env.MIX_OPENSSL_SECRET_IV;
+
+function encrypt_token(data) {
+    let cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+    cipher.update(data, 'binary', 'base64');
+    return cipher.final('base64');
+}
+
+function decrypt_token (data) {
+    let decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+    decipher.update(data, 'base64', 'binary');
+    return decipher.final('binary');
+}
+
+function test(){
+    alert('test')
+}
+
+let text = '0812-8888-9999';
+console.log('NodeJS encrypt: ', encrypt_token(text));
+console.log('NodeJS decrypt: ', decrypt_token('yHsFnmWk+I3AV2F2ZAQGqA==')); //'dmyz.org'
+
+
 //import crypto from '/crypto-browserify'
 //import { Buffer } from '/buffer' // <-- no typo here ("/")
 /*
