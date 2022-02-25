233947b93d2723e41f650198d0d7d4a5fedd19ed
LutviP19 <lutvip19@gmail.com>
2022-02-26 06:28:27 +0700

    Fix Toast

--- resources/js/main-notify.js
+++ resources/js/main-notify.js
@@ -1,13 +1,21 @@
 
+function showToast(message) {
+    var toast = document.getElementById('toast-default');
+    var toastText = document.getElementById('toast-text-default');
+    var x = document.getElementById("notifAudio");
+
+    toastText.innerHTML = message;
+    toast.style.display = 'block';
+    x.play();
+}
+
+
 Echo.channel('process-userdata')
     .listen('.ProcessUserdataEvent', (data) => {
         // this is called when the event notification is received...
-        //alert(JSON.stringify(data));
         //console.log('.ProcessUserdataEvent = ' + JSON.stringify(data));
 
         if(data.userdata) {
-            var toast = document.getElementById('toast-default');
-            var toastText = document.getElementById('toast-text-default');
             var tableGanjil = document.getElementById("tblGanjil");
             //var tblBodyGanjil = document.getElementById('tblBodyGanjil');
             var tableGenap = document.getElementById("tblGenap");
@@ -22,22 +30,16 @@ Echo.channel('process-userdata')
             cell2.innerHTML = '<a href="#" class="text-blue-600 hover:text-blue-900 dark:text-blue-500 dark:hover:underline">Edit</a> - ' +
                               '<a href="#" class="text-red-600 hover:text-red-900 dark:text-red-500 dark:hover:underline">Delete</a>';
 
-            toastText.innerHTML = 'Number ' + data.userdata.phone_number + ' successfully saved!';
-            toast.style.display = 'block';
-
-            var x = document.getElementById("notifAudio");
-            x.play();
+            showToast('Number ' + data.userdata.phone_number + ' successfully saved!');
         }
     });
 
 Echo.channel('process-auto-userdata')
     .listen('.ProcessAutoUserdataEvent', (data) => {
         // this is called when the event notification is received...
-        //alert(JSON.stringify(data));
-        console.log('.ProcessAutoUserdataEvent = ' + JSON.stringify(data));
+        //console.log('.ProcessAutoUserdataEvent = ' + JSON.stringify(data));
+
         if(data.data) {
-            var toast = document.getElementById('toast-default');
-            var toastText = document.getElementById('toast-text-default');
             var tableGanjil = document.getElementById("tblGanjil");
             //var tblBodyGanjil = document.getElementById('tblBodyGanjil');
             var tableGenap = document.getElementById("tblGenap");
@@ -46,24 +48,18 @@ Echo.channel('process-auto-userdata')
             let arr = data.data;
             for (var i = 0; i < arr.length; i++){
                 var obj = arr[i];
-                //for (var key in obj) {
-                    //var value = obj[key];
-                    var row = obj['number_type'] == 'ganjil' ? tableGanjil.insertRow(-1) : tableGenap.insertRow(-1);
-                    var cell1 = row.insertCell(0);
-                    var cell2 = row.insertCell(1);
-                    row.className = 'bg-white border-b dark:bg-gray-800 dark:border-gray-700';
-                    cell1.className = 'py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white';
-                    cell2.className = 'py-4 px-6 text-sm font-medium text-right whitespace-nowrap';
-                    cell1.innerHTML = obj['phone_number'];
-                    cell2.innerHTML = '<a href="#" class="text-blue-600 hover:text-blue-900 dark:text-blue-500 dark:hover:underline">Edit</a> - ' +
-                                    '<a href="#" class="text-red-600 hover:text-red-900 dark:text-red-500 dark:hover:underline">Delete</a>';
-                //}
+                var row = obj['number_type'] == 'ganjil' ? tableGanjil.insertRow(-1) : tableGenap.insertRow(-1);
+                var cell1 = row.insertCell(0);
+                var cell2 = row.insertCell(1);
+                row.className = 'bg-white border-b dark:bg-gray-800 dark:border-gray-700';
+                cell1.className = 'py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white';
+                cell2.className = 'py-4 px-6 text-sm font-medium text-right whitespace-nowrap';
+                cell1.innerHTML = obj['phone_number'];
+                cell2.innerHTML = '<a href="#" class="text-blue-600 hover:text-blue-900 dark:text-blue-500 dark:hover:underline">Edit</a> - ' +
+                                '<a href="#" class="text-red-600 hover:text-red-900 dark:text-red-500 dark:hover:underline">Delete</a>';
             }
 
-            toastText.innerHTML = '500 Random Number successfully generated!';
-            toast.style.display = 'block';
-            var x = document.getElementById("notifAudio");
-            x.play();
+            showToast('500 Random Number successfully generated!');
         }
     });
 
